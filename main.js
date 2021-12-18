a1b539b1e857a16c2b53efb2743465fde6f2fd97
Neemias Renan <neemiasrenan2015@gmail.com>
2021-12-18 17:59:35 -0300

    Atualizações

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 ids = 0
 somadosvalores = 0
 
+var dadosdosprodutos = ['613|Papel Toalha Essencial|10.50', '828|Papel Aluminio|4.50', 'c64|Arames - Amarrilho|29.00', '947|Caixa de Pizza n30|41.00', '172|Caixa de Pizza n35|42.00', '585|Filme PVC 38*600mt|66.00', '735|Filme PVC 100m|12.50', '660|Filme PVC 300m|29.90', '422|Etiqueta de Preço 1uni|1.80', 'd08|Etiqueta de Preço 10uni|16.50', '88b|Etiqueta de Preço 5uni|8.50', '507|Etiqueta de Peso 5uni|27.50', '024|Etiqueta de Peso 1uni|6.00', '475|Guardanapos Solis|0.85', '44e|Guardanapos Reserva|0.90']
+var codigos = ['613', '828', 'c64', '947', '172', '585', '735', '660', '422', 'd08', '88b', '507', '024', '475', '44e']
+
 function atualizarvalor(){
     document.getElementById('somatotal').innerHTML = somadosvalores
     calculartroco()
@@ -19,6 +22,18 @@ function addvalor(valor){
         li.setAttribute('id','li_'+ids+'')
         li.setAttribute('class','listadeprodutos-li')
         ul.append(li)
+
+        p = document.createElement('p')
+        p.setAttribute('id','p_'+ids+'')
+        p.setAttribute('class','listadeprodutos_li-p')
+        p.innerText = "==="
+        li.append(p)
+
+        p = document.createElement('p')
+        p.setAttribute('id','p_'+ids+'')
+        p.setAttribute('class','listadeprodutos_li-p')
+        p.innerText = "==="
+        li.append(p)
 
         p = document.createElement('p')
         p.setAttribute('id','p_'+ids+'')
@@ -95,24 +110,136 @@ function atualizarquantidadepadrao(){
     }
 }
 
-var produtos = []
-var codigos = []
-function addcodigo(nome){
+function addproduto(produto){
+    codigodoproduto =document.getElementById(produto).value
+    
+    informacoesdoproduto = procurarproduto(codigodoproduto)
+
+    arrayinfodoproduto = informacoesdoproduto.split('|')
+
+    console.log(arrayinfodoproduto)
+
+    nomedoproduto = arrayinfodoproduto[1]
+    valor = parseFloat(arrayinfodoproduto[2]).toFixed(2)
+    quantidade = parseInt(document.getElementById("inputquantidadepelocodigo").value)
+
+    valor = parseFloat(valor*quantidade).toFixed(2)
+    
+    if(valor!="NaN" & valor>0){
+        ul = document.getElementById('listadeprodutos')
+        li = document.createElement('li')
+        li.setAttribute('id','li_'+ids+'')
+        li.setAttribute('class','listadeprodutos-li')
+        ul.append(li)
+
+        p = document.createElement('p')
+        p.setAttribute('id','p_nome'+ids+'')
+        p.setAttribute('class','listadeprodutos_li-p')
+        p.innerText = nomedoproduto
+        li.append(p)
+
+        p = document.createElement('p')
+        p.setAttribute('id','p_quantidade'+ids+'')
+        p.setAttribute('class','listadeprodutos_li-p')
+        p.innerText = quantidade
+        li.append(p)
+
+        p = document.createElement('p')
+        p.setAttribute('id','p_'+ids+'')
+        p.setAttribute('class','listadeprodutos_li-p')
+        p.innerText = "R$"+valor
+        li.append(p)
+
+        button = document.createElement('input')
+        button.setAttribute('type','button')
+        button.setAttribute('value','x')
+        button.setAttribute("onclick","apagarvalor('li_"+ids+"','p_"+ids+"')")
+        li.append(button)
+
+        somadosvalores = (parseFloat(somadosvalores)+parseFloat(valor)).toFixed(2)
+        ids = ids+1
+        atualizarvalor()
+        document.getElementById('inputcodigo').value=''
+        document.getElementById('inputquantidadepelocodigo').value='1'
+    }
+    
+}
+
+
+// ´-----------
+
+
+
+function addcodigo(){
     code = Math.random().toString(16).substr(2, 3)
     const procurar = codigos.find(element => element == code);
-    
-    document.getElementById('inputcodigo').value = code
-
-    codigos.push(code)
-
-
-    console.log(code, procurar)
+    if (procurar == undefined){
+        document.getElementById('inputcodigo').value = code
+    }else{
+        addcodigo()
+    }
 }
 
-function procurar(){
+function addnoestoque(produto,codigo,preco){
+
+    const procurar = codigos.find(element => element == code);
+
+    nomedoproduto = document.getElementById(produto).value
+    codigodoproduto = document.getElementById(codigo).value
+    precodoproduto = parseFloat(document.getElementById(preco).value).toFixed(2)
+
+    dadoscompletos = codigodoproduto + "|" + nomedoproduto+'|'+precodoproduto
+
+    dadosdosprodutos.push(dadoscompletos)
+    codigos.push(codigodoproduto)
+
+    console.log(dadosdosprodutos)
     
+    document.getElementById(produto).value = ''
+    document.getElementById(codigo).value = ''
+    document.getElementById(preco).value = ''
+    atualizarlistadoestoque()
 }
 
-// produto = document.getElementById(nome).value
-// produtos.push(produto)
-// console.log(produtos)
+function procurarproduto(codigo){
+    
+    posicaonoarray = codigos.indexOf(codigo)
+    // const posicaonoarray = codigos.find(element => element === codigo);
+    
+    dados = dadosdosprodutos[posicaonoarray]
+    return dados
+
+}
+
+function atualizarlistadoestoque(){
+    id_quantidadedeprodutos = 0
+    ul = document.getElementById('listadeprodutos').innerHTML = ''
+    interacao = dadosdosprodutos.length
+    console.log(interacao)
+    for(interacao<=0; interacao--;){
+        ul = document.getElementById('listadeprodutos')
+        li = document.createElement('li')
+        li.setAttribute('id','li_'+id_quantidadedeprodutos +'')
+        li.setAttribute('class','listadeprodutos-li')
+        ul.append(li)
+
+        p = document.createElement('p')
+        p.setAttribute('id','p_'+id_quantidadedeprodutos +'')
+        p.setAttribute('class','listadeprodutos_li-p')
+        p.innerText = dadosdosprodutos[interacao]
+        li.append(p)
+
+        button = document.createElement('input')
+        button.setAttribute('type','button')
+        button.setAttribute('value','x')
+        button.setAttribute("onclick","apagarprodutodoestoque('li_"+ids+"')")
+        li.append(button)
+        console.log(interacao)
+        id_quantidadedeprodutos = id_quantidadedeprodutos +1
+    }
+}   
+
+function apagarprodutodoestoque(id){
+    id_element = document.getElementById(id)
+    id_element.remove()
+}
