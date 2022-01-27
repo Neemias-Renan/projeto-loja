44a1476d817dc25d3bc9ee86462602a599497e2e
Neemias Renan <neemiasrenan2015@gmail.com>
2022-01-26 23:53:12 -0300

    atualizações do js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 // Botão limpar
-var valorfinal = 0.00
+valorfinal = 0
+ids = 0
 
 function limpar(){
     let lista = document.getElementsByClassName('lista')
@@ -11,30 +12,74 @@ function limpar(){
         x.remove()
         cont--
     }
+    document.getElementById('valordoproduto').value = ""
+    document.getElementById('quantidadedoproduto').value = "1"
+    document.getElementById('ValorFinal').innerHTML = "00.00"
+    valorfinal = 0
 }
 
 function remover(id){
-    x = document.getElementById(id)
-    x.remove()
-}
+    tr = document.getElementById(id)
+    valor = document.getElementById(id+"_valor").innerText
+    valorfinal = parseFloat(parseFloat(valorfinal)-parseFloat(valor)).toFixed(2)
+    document.getElementById('ValorFinal').innerHTML = valorfinal
+    tr.remove()
 
-function confirmar(valor,quantidade){
-    valordoProduto = parseFloat(valor.value).toFixed(2)
-    quantidadedoProduto = parseFloat(quantidade.value).toFixed(2)
-
-    criarNovaTR()
-
-    console.log(valordoProduto)
-    console.log(quantidadedoProduto)
 }
 
 
-function criarNovaTR(){
-    let lista = document.getElementsByClassName('lista')
-    let quantidade = lista.length
+function add(valor,quantidade){
+    valor = parseFloat(document.getElementById(valor).value).toFixed(2)
+    quantidade = parseFloat(document.getElementById(quantidade).value).toFixed(2)
+    if(valor!="NaN" && quantidade!="NaN" && quantidade!=0.00){
+       
+        tbody = document.getElementById('tabela_carrinho')
+        tr = document.createElement('tr')
+        tr.setAttribute('id','tr'+ids+'')
+        tr.setAttribute('class','trTabela marginTop lista')
+        tbody.append(tr)
+        
+        th1 = document.createElement('th')
+        th1.setAttribute('class','thDescricao')
+        th1.innerText = "R$"
+        tr.append(th1)
 
-    tbody = document.getElementsByTagName('tbody')
+        span = document.createElement('span')
+        span.setAttribute('id','tr'+ids+'_valor')
+        span.innerText = parseFloat(valor*quantidade).toFixed(2)
+        th1.append(span)
 
-    tr = document.createElement('tr')
-    tbody.appendChild(tr)
+        th2 = document.createElement('th')
+        th2.setAttribute('class','thDescricao')
+        th2.innerText = parseInt(quantidade) +" Unidades"
+        tr.append(th2)
+
+        th3 = document.createElement('th')
+        tr.append(th3)
+
+        button = document.createElement('button')
+        button.setAttribute('class','botaoRemover')
+        button.setAttribute("onclick","remover('tr"+ids+"')")
+        th3.append(button)
+
+        img = document.createElement('img')
+        img.setAttribute('src','src/img/remove.svg')
+        button.append(img)
+
+        ids += 1
+    
+        valorfinal = parseFloat(parseFloat(valorfinal)+parseFloat(valor*quantidade)).toFixed(2)
+        document.getElementById('ValorFinal').innerHTML = valorfinal
+    }
+    if(valor!="NaN" && quantidade!="NaN" && quantidade!=0.00){
+        document.getElementById('valordoproduto').value = ""
+        document.getElementById('quantidadedoproduto').value = "1"
+    }
+}
+
+
+function calcularTroco(){
+    valorpago = document.getElementById('inputValorPago').value
+    valorfinal = parseFloat(valorpago)-parseFloat(valorfinal)
+    console.log(valorfinal)
 }
